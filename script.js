c40f4e44c791c1fe7684b7c423c2f8b31441f20b
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-09-15 19:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
+ var team = players;
+
+    // Create a shallow copy of the 'players' array
+    var team1 = players.slice();
+
+    // Create a shallow copy of the 'person' object
+    var cap1 = Object.assign({}, person);
+
+    // You can now use 'team', 'team1', and 'cap1' as needed
+    console.log("Team:", team);
+    console.log("Team1:", team1);
+    console.log("Cap1:", cap1);
